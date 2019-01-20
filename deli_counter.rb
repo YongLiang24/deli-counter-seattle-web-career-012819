02c70e701c917c3b3e68ac606faa5b0c3e5aeb49
yong liang <yongliang24@gmail.com>
2019-01-20 03:55:53 +0000

    Automatically backed up by Learn

--- deli_counter.rb
+++ deli_counter.rb
@@ -1,9 +1,26 @@
 # Write your code here.
 def line(name_array)
+  position=1
+  empty_array=[]
   if name_array.size<1
     puts "The line is currently empty."
+  else
+    name_array.each do |name|
+      empty_array.push()
+      position+=1
+    end
   end
 
+
+  context "there are people in line" do
+    it "should display the current line" do
+      expect($stdout).to receive(:puts).with("The line is currently: 1. Logan 2. Avi 3. Spencer")
+      line(other_deli)
+    end
+
+    it 'works with different people in line' do
+      expect($stdout).to receive(:puts).with("The line is currently: 1. Amanda 2. Annette 3. Ruchi 4. Jason 5. Logan 6. Spencer 7. Avi 8. Joe 9. Rachel 10. Lindsey")
+  line(another_deli)
 
   if name_array.size > 0
     nuarray = []
