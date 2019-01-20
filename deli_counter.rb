f29405f4076fe5421df290f26403f4c6b632c9cd
yong liang <yongliang24@gmail.com>
2019-01-20 04:09:34 +0000

    Automatically backed up by Learn

--- deli_counter.rb
+++ deli_counter.rb
@@ -17,3 +17,12 @@ def take_a_number(array, name)
   array.push(name)
   puts "Welcome, #{name}. You are number #{array.size} in line."
 end
+
+def now_serving(array)
+  if array.size<1
+    puts "There is nobody waiting to be served!"
+  else
+    puts "Currently serving #{array[0]}."
+    array.shift
+  end
+end
